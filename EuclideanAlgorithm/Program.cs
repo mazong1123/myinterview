151857d1713335d99f88f5cb8e690327866b5648
mazong1123 <mazong1123@gmail.com>
2016-10-10 20:13:04 +0800

    Added EuclideanAlgorithm sample.

---
+++ EuclideanAlgorithm/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EuclideanAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 1020;
+            int second = 60;
+
+            int commonDivisor = GetCommonDivisor(first, second);
+
+            Console.WriteLine(commonDivisor);
+
+            Console.ReadLine();
+        }
+
+        static int GetCommonDivisor(int first, int second)
+        {
+            int big = first;
+            int small = second;
+
+            if (big < small)
+            {
+                big = second;
+                small = first;
+            }
+
+            if (big % small == 0)
+            {
+                return small;
+            }
+
+            int newResult = big % small;
+
+            return GetCommonDivisor(newResult, small);
+        }
+    }
+}
