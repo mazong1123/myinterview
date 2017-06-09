01c12234ed45520a89da1bcc22a38baf4fbe6333
Jim Ma <mazong1123@gmail.com>
2017-06-09 13:04:49 +0800

    Update longestnonrepeating.cpp

--- misc/longestnonrepeating.cpp
+++ misc/longestnonrepeating.cpp
@@ -1,30 +1,27 @@
-
-int lengthOfLongestSubstring(string s) 
-{
-  if (s.empty())
-  {
-    return 0;
-  }        
-
-  int res = -1;
-  int start = 0;
-  int end = 0;
-  unordered_map<char, int> ht;
-
-  while (end < s.length())
-  {
-    if (ht.find(s[end]) != ht.end())
-    {
-      res = max(res, end - start);
-      start = max(start, ht[s[end]] + 1);
-      start = ht[s[end]] + 1;
+    int lengthOfLongestSubstring(string s) {
+    	if (s.empty())
+    	{
+    		return 0;
+    	}        
+    
+    	int res = -1;
+    	int start = 0;
+    	int end = 0;
+    	unordered_map<char, int> ht;
+    
+    	while (end < s.length())
+    	{
+    		if (ht.find(s[end]) != ht.end())
+    		{
+    			res = max(res, end - start);
+    			start = max(start, ht[s[end]] + 1);
+    		}
+    		
+    		ht[s[end]] = end;
+    		++end;
+    	}
+	
+    	res = max(res, end - start);
+    
+    	return res;
     }
-
-    ht[s[end]] = end;
-    ++end;
-  }
-
-  res = max(res, end - start);
-
-  return res;
-}
